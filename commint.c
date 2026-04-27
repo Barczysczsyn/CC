7b211682b07191df3df861ac3fda2e7b0465de0f
Barczysczsyn <jmbvilha@gmail.com>
2026-04-26 22:29:24 -0400

    fiz do commint

--- commint.c
+++ commint.c
@@ -6,98 +6,131 @@
 #include <sys/wait.h>
 #include <dirent.h>
 #include <errno.h>
+#include <ctype.h>
 
 #define true 1
 #define false 0
 #define MAX_LINHA 100
 #define MAX_COMANDOS 50
 
-void printaArvore(char caminho[50], int rec)
+typedef struct
 {
-    FILE *arqv = fopen(caminho, "r");
+    int pid;
+    char nome[256];
+    char estado;
+    int ppid;
+} Proc;
 
-    char argumentos[4][50];
-    // o pai é o argumentos 3
-    fscanf(arqv, "%d (%[^)]) %c %d",
-           argumentos[0],
-           argumentos[1],
-           argumentos[2],
-           argumentos[3]);
-
-    // imprimir esse arquivo
-    printf("\n");
-    for (int i = 0; i < rec; i++)
-        printf(" ");
-    // resolvi colocar assim
-    printf("|-%d %s %c %d", argumentos[0], argumentos[1], argumentos[2], argumentos[3]);
-}
-void acharFilhos(char pai[10], int rec)
+void imprimirArvore(int atual, Proc *procs, int proccont, int rec)
 {
-
-    // abrir a pasta onde estao os arquivos
-    DIR *pasta = opendir("/proc");
-    if (pasta == NULL)
+    for (int i = 0; i < proccont; i++)
     {
-        printf("Error: Não foi possivel abrir a pasta.");
-    }
-    struct dirent *de;
-
-    // ABRIR UM ARQUIVO
-    while ((de = readdir(pasta)) != NULL)
-    {
-        // de = readdir(pasta);
-        // printf("\n arquivo '%s'", de->d_name);
-        //  fflush(stdout);
-
-        // checa se o nome da pasta é um numero,e se nao é o proprio pai
-        if ((de->d_name[strspn(de->d_name, "0123456789")] == '\0') && strcmp(de->d_name, pai) != 0)
+        // o processo é filho desse
+        if (procs[i].pid == atual)
         {
-
-            char caminho[50] = "/proc/";
-            strcat(caminho, de->d_name);
-            strcat(caminho, "/stat");
-
-            FILE *arqv = fopen(caminho, "r");
-
-            if (arqv == NULL)
-            { // Verify the file opened successfully
-                printf("\nErro, arquivo não encontrado.");
-                fflush(stdout);
-                return;
-            }
-
-            // printf("\nbuffer %s", buffer);
-            // fflush(stdout);
-            FILE *arqv = fopen(caminho, "r");
-
-            char argumentos[4][50];
-            // o pai é o argumentos 3
-            fscanf(arqv, "%d (%[^)]) %c %d",
-                   argumentos[0],
-                   argumentos[1],
-                   argumentos[2],
-                   argumentos[3]);
-
-            // tem q ser assim
-
-            if (strcmp(argumentos[3], pai) == 0)
+            for (int j = 0; j < rec; j++)
             {
-                // printf("\n checar %s",de->d_name);
-                //  tal arquivo é filho desse
-                // chamar recursivamente para achar os filhos dele tbm
-                acharFilhos(de->d_name, rec + 1);
-
-                // TODO existem nomes de arquivos com espaços
+                printf(" ");
+            }
+            if (rec > 0)
+            {
+                printf("|-");
             }
 
-            // fechar o arquivo, agora que arqc nao esta mais sendo usado
-            fclose(arqv);
+            char estado[25];
+
+            switch (procs[i].estado){
+
+                case 'R':
+                strcpy(estado,"Running ou Runnable");
+                break;
+                case 'S':
+                strcpy(estado,"Interruptible Sleep");
+                break;
+                case 'D':
+                strcpy(estado,"Uninterruptible Sleep");
+                break;
+                case 'I':
+                strcpy(estado,"Idle");
+                break;
+                case 'T':
+                strcpy(estado,"Stopped or Traced");
+                break;
+                case 'X':
+                strcpy(estado,"Dead");
+                break;
+                case 'Z':
+                strcpy(estado,"Zombie");
+                break;
+            }
+
+            printf("%d | %s | %s\n", procs[i].pid, procs[i].nome, estado);
+
+            for (int k = 0; k < proccont; k++)
+            {
+                if (procs[k].ppid == atual)
+                {
+                    imprimirArvore(procs[k].pid, procs, proccont, rec + 1);
+                }
+            }
+            return;
         }
     }
-    // ABRIR UM ARQUIVO
-
-    // pega o pai dele
 }
+
+void acharFilhos(int pai)
+{
+    DIR *pastaAlvo;
+    struct dirent *de;
+    Proc *procs = malloc(sizeof(Proc) * 1000);
+    int proccont = 0;
+
+    if (procs == NULL)
+    {
+        perror("malloc do procs");
+        return;
+    }
+
+    if ((pastaAlvo = opendir("/proc")) != NULL)
+    {
+        while ((de = readdir(pastaAlvo)) != NULL)
+        {
+            if (isdigit(de->d_name[0]))
+            {
+                char path[520];
+                snprintf(path, sizeof(path), "/proc/%s/stat", de->d_name);
+
+                FILE *arqv = fopen(path, "r");
+                if (arqv)
+                {
+                    if (fscanf(arqv, "%d (%[^)]) %c %d",
+                               &procs[proccont].pid,
+                               procs[proccont].nome,
+                               &procs[proccont].estado,
+                               &procs[proccont].ppid) == 4)
+                    {
+                        proccont++;
+                    }
+                    fclose(arqv);
+                }
+            }
+        }
+        closedir(pastaAlvo);
+
+        printf("\nArvore de processos de %d:\n", pai);
+
+        printf("\nNumero de PID | Nome | Estado\n");
+
+        imprimirArvore(pai, procs, proccont, 0);
+    }
+    else
+    {
+        perror("\nErro ao abrir proc");
+    }
+
+    free(procs);
+}
+
 int main()
 {
 
@@ -179,11 +212,6 @@ int main()
             // return 0;
             //  exit(0);
         }
-        else if (strcmp(comandos[0], "cd") == 0)
-        {
-            chdir(comandos[1]);
-            // opendir(comandos[1]);
-        }
         else if (strcmp(comandos[0], "tree") == 0)
         {
             char pastaAlvo[50] = "/proc/";
@@ -204,7 +232,12 @@ int main()
             // Read until fgets returns NULL (end of file)
             fgets(buffer, 50, fptr);
             // printf("%s", buffer);
-            acharFilhos(comandos[1], 1);
+            acharFilhos(atoi(comandos[1]));
+        }
+        else if (strcmp(comandos[0], "cd") == 0)
+        {
+            chdir(comandos[1]);
+            // opendir(comandos[1]);
         }
         else
 
@@ -215,17 +248,19 @@ int main()
             pid = fork();
             if (pid < 0)
             { // error occurred
-                fprintf(stderr, "Fork Failed");
+                fprintf(stderr, "Fork falhou");
                 exit(-1);
             }
             else if (pid == 0)
             { // child process
-                execvp(comandos[0], comandos);
+                if(execvp(comandos[0], comandos) == -1){
+                    perror("\nErro ao executar programa");
+                    exit(0);
+                }
             }
             else
             { // parent process
                 // parent will wait for the child to complete
-                // TODO se certificar de que isso funciona
                 if (back == 0)
                 {
                     wait(NULL);
