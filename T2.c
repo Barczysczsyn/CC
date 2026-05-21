bf703698e2a9bc4be5c67102a5102232212114b4
Barczysczsyn <jmbvilha@gmail.com>
2026-05-20 23:52:21 -0400

    alguns ngv

--- T2.c
+++ T2.c
@@ -16,23 +16,25 @@ void printaLista(struct s_no *no)
     {
         printf("\n");
 
-        printf("%d o-> ", no->num);
-        no = no->prox;
-        while ((no != NULL))
+        printf("%d -o-> ", no->num);
+        struct s_no *p1 = no->prox;
+        while ((p1 != NULL))
         {
-            printf("%d o-> ", no->num);
-            no = no->prox;
+            printf("%d -o-> ", p1->num);
+            p1 = p1->prox;
         }
     }
+    fflush(stdout);
 }
 void inserir(struct s_no **pont, int num)
 {
+
+    struct s_no *novo = malloc(sizeof(struct s_no));
+    novo->num = num;
+    novo->prox = NULL;
+
     if (*pont == NULL)
     {
-
-        struct s_no *novo = malloc(sizeof(struct s_no));
-        novo->num = num;
-        novo->prox = NULL;
 
         (*pont) = novo;
     }
@@ -40,55 +42,96 @@ void inserir(struct s_no **pont, int num)
     {
 
         // ir ate o final da lista
-        while ((*pont)->prox != NULL)
+        // aparentemente precisa de um auxiliar
+        struct s_no *atual = *pont;
+        while (atual->prox != NULL)
         {
-            *pont = (*pont)->prox;
+            atual = atual->prox;
         }
 
-        struct s_no *novo = malloc(sizeof(struct s_no));
-        novo->num = num;
-        novo->prox = NULL;
-
-        (*pont)->prox = novo;
+        atual->prox = novo;
     }
 }
-void remover(struct s_no **no, int num)
+void remover(struct s_no **pont, int num)
 {
-    struct s_no *ant;
-    // achar o no
-    while (((*no)->num != num) && ((*no)->prox != NULL))
+    if (*pont == NULL)
     {
-        ant = *no;
-        *no = (*no)->prox;
+
+        return;
     }
-
-    // teoricamente funciona
-    ant->prox = (*no)->prox;
-    // free(*no);
-}
-void removerPares(struct s_no **no)
-{
-    // remove pares diferentes de 2
-    struct s_no *ant;
-    // achar o no
-    while ((*no)->prox != NULL)
+    else if ((*pont)->num == num)
     {
-        ant = *no;
-        *no = (*no)->prox;
+        struct s_no *temp = *pont;
+        *pont = (*pont)->prox;
+        free(temp);
+        return;
+    }
+    else
+    {
+        struct s_no *ant = *pont, *p1 = (*pont)->prox;
+        // achar o no
+        while ((p1 != NULL) && (p1->num != num))
+        {
+            ant = p1;
+            p1 = p1->prox;
+        }
 
-        if (((*no)->num % 2 == 0) && ((*no)->num != 2))
+        // se o no for achado
+        if (p1->num == num)
         {
 
-            // teoricamente funciona
-            ant->prox = (*no)->prox;
-            free(*no);
+            // printf("\nremovido %d", p1->num);
+            // fflush(stdout);
+            //  teoricamente funciona
+            ant->prox = p1->prox;
+            free(p1);
         }
+    }
+}
+void removerPares(struct s_no **pont)
+{
+    if (*pont == NULL)
+    {
+
+        return;
+    }
+    else if (((*pont)->num % 2 == 0) && ((*pont)->num != 2))
+    {
+        struct s_no *temp = *pont;
+        *pont = (*pont)->prox;
+        free(temp);
+        return;
+    }
+    else
+    {
+        struct s_no *ant = *pont, *p1 = (*pont)->prox;
+        // achar o no
+        while (p1 != NULL)
+        {
+            if ((p1->num % 2 == 0) && (p1->num != 2))
+            {
+
+                // printf("\nremovido %d", p1->num);
+                // fflush(stdout);
+                //TODO testar
+                //  teoricamente funciona
+                ant->prox = p1->prox;
+
+                struct s_no *temp = p1;
+                p1 = p1->prox;
+                free(temp);
+            }
+            ant = p1;
+            p1 = p1->prox;
+        }
+
+        // se o no for achado
     }
 }
 
 int main()
 {
-    struct s_no *L;
+    struct s_no *L = NULL;
     // L->prox = NULL;
 
     // teste
@@ -97,17 +140,18 @@ int main()
     inserir(&L, 3);
     inserir(&L, 4);
     printaLista(L);
-    fflush(stdout);
-    remover(&L, 2);
+    removerPares(&L);
     printaLista(L);
 
     FILE *arqv = fopen("in.txt", "r");
 
-    int *numeros;
-    while (fread(numeros, sizeof(int), 1, arqv))
-    {
-        inserir(&L, *numeros);
-    }
+    // mede o tamanho
+    fseek(arqv, 0, SEEK_END); // Seek to the end
+    long tam = ftell(arqv);   // Get current position
+
+    int *numeros = malloc(tam / sizeof(int));
+    fread(numeros, sizeof(int), 1, arqv);
+    inserir(&L, *numeros);
 
     // 1 thread
 
